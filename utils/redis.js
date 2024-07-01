cbe9f3e2069936dad4954ec96631a9a4232c80d2
Ermiyas <ermiyasabiye1999@gmail.com>
2024-07-01 11:20:46 +0300

    connect to redis client class

---
+++ utils/redis.js
@@ -0,0 +1,33 @@
+const redis = require('redis');
+const { promisify } = require('util');
+
+class RedisClient {
+  constructor() {
+    this.client = redis.createClient();
+
+    this.client.on('error', (err) => {
+      console.log('Redis Client Error', err);
+    });
+  }
+
+  isAlive() {
+    return this.client.connected;
+  }
+
+  async get(key) {
+    const getAsync = promisify(this.client.get).bind(this.client);
+    return getAsync(key);
+  }
+
+  async set(key, value, duration) {
+    await this.client.setex(key, duration, value);
+  }
+
+  async del(key) {
+    await this.client.del(key);
+  }
+}
+
+const redisClient = new RedisClient();
+
+module.exports = redisClient;
