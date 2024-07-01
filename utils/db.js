9e677e41cea955eb6654fd5c34aa0d2444ecd19d
Ermiyas <ermiyasabiye1999@gmail.com>
2024-07-01 15:29:03 +0300

    connect to mongodb

---
+++ utils/db.js
@@ -0,0 +1,34 @@
+const process = require('process');
+const { MongoClient } = require('mongodb');
+
+const host = process.env.DB_HOST || 'localhost';
+const port = process.env.DB_PORT || 27017;
+const database = process.env.DB_DATABASE || 'files_manager';
+
+class DBClient {
+  constructor() {
+    this.db = null;
+    const url = `mongodb://${host}:${port}`;
+
+    MongoClient.connect(url, (error, client) => {
+      if (error) console.log(error);
+      this.db = client.db(database);
+    });
+  }
+
+  isAlive() {
+    return !!this.db;
+  }
+
+  async nbUsers() {
+    return this.db.collection('users').countDocuments();
+  }
+
+  async nbFiles() {
+    return this.db.collection('files').countDocuments();
+  }
+}
+
+const dbClient = new DBClient();
+
+export default dbClient;
