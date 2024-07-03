13547d911a816dc4f203163ddb45f9f238a1e1f0
Ermiyas <ermiyasabiye1999@gmail.com>
2024-07-04 00:40:18 +0300

    file data

--- routes/index.js
+++ routes/index.js
@@ -49,6 +49,9 @@ const routeController = (app) => {
   router.put('/files/:id/unpublish', (req, res) => {
     FilesController.putUnpublish(req, res);
   });
+  router.get('/files/:id/data', (req, res) => {
+    FilesController.getFile(req, res);
+  });
 };
 
 export default routeController;
